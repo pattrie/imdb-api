45bfd95876af99203fa9533e442a30ba63d8e3f6
pattrie <patri.b.morais@gmail.com>
2022-04-03 02:44:16 -0300

    add project

--- src/main/java/com/github/pattrie/imdbapi/ImdbApiApplication.java
+++ src/main/java/com/github/pattrie/imdbapi/ImdbApiApplication.java
@@ -1,36 +1,57 @@
 package com.github.pattrie.imdbapi;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImdbApiApplication {
 
-	public static void main(String[] args) throws URISyntaxException {
-//		SpringApplication.run(ImdbApiApplication.class, args);
+  public static void main(String[] args) throws Exception {
+    //SpringApplication.run(ImdbApiApplication.class, args);
 
-		Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Insira a apiKey: " );
-		String apiKey = scanner.nextLine();
+    System.out.print("Insira a apiKey: ");
+    String apiKey = scanner.nextLine();
 
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(new URI(String.format("https://imdb-api.com/en/API/Top250Movies/%s", apiKey)))
-				.GET()
-				.build();
+    final URI uri = new URI(String.format("https://imdb-api.com/en/API/Top250Movies/%s", apiKey));
+    HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
 
-		HttpClient client = HttpClient.newHttpClient();
+    HttpClient client = HttpClient.newHttpClient();
 
-		client.sendAsync(request, BodyHandlers.ofString())
-				.thenApply(HttpResponse::body)
-				.thenAccept(System.out::println)
-				.join();
-	}
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    final String[] movies = getMovies(response.body());
+
+    System.out.println(getList(movies, "title"));
+    System.out.println(getList(movies, "image"));
+    System.out.println(getList(movies, "imDbRatingCount"));
+    System.out.println(getList(movies, "year"));
+  }
+
+  private static List getList(String[] movies, String information) {
+    List<String> list = new ArrayList<>();
+
+    for (String movie : movies) {
+      if (movie.contains(information)) {
+        final int beginIndex = movie.indexOf("\":\"") + 3;
+        final int endIndex = movie.lastIndexOf("\"");
+        final int finalIndex = endIndex < beginIndex ? movie.length() : endIndex;
+        list.add(movie.substring(beginIndex, finalIndex));
+      }
+    }
+    return list;
+  }
+
+  private static String[] getMovies(String response) {
+    final String body = response;
+    body.substring(body.indexOf("[") + 1, body.indexOf("]"));
+    return body.split(",");
+  }
 }
